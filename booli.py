d91d585ca94e5907f84a885861e26ddf5dd2e969
willbengt <wilbe075@student.liu.se>
2017-04-21 03:29:47 +0200

    Added callbooliapi function

--- booli.py
+++ booli.py
@@ -6,51 +6,65 @@ import random
 import string
 import json
 
-callerId = "williambengtsson"
-timestamp = str(int(time.time()))
-unique = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
-s = callerId+timestamp+"1B8yJRDjHUQwND92jnQ3awLOCaj6NxFfi3HInjN4"+unique
-hashstr = sha1(s.encode('utf-8')).hexdigest()
+def callbooliapi(limit, query, offset):
+  callerId = "williambengtsson"
+  timestamp = str(int(time.time()))
+  unique = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(16))
+  s = callerId+timestamp+"1B8yJRDjHUQwND92jnQ3awLOCaj6NxFfi3HInjN4"+unique
+  hashstr = sha1(s.encode('utf-8')).hexdigest()
+  
+  url = "/sold?q="+query+"&limit="+str(limit)+"&offset="+str(offset)+"&callerId="+callerId+"&time="+timestamp+"&unique="+unique+"&hash="+hashstr
+  
+  # connection = http.client.HTTPConnection("api.booli.se") #python 3
+  connection = httplib.HTTPConnection("api.booli.se") #python 2
+  connection.request("GET", url)
+  response = connection.getresponse()
+  data = response.read()
+  connection.close() 
+
+  if response.status != 200:
+    print("fail")
+
+  return data
+
+
 
 limit = 1000
 query = "vasastan"
 offset = 0
 
-url = "/sold?q="+query+"&limit="+str(limit)+"&offset="+str(offset)+"&callerId="+callerId+"&time="+timestamp+"&unique="+unique+"&hash="+hashstr
+result = callbooliapi(limit, query, offset)
 
-# connection = http.client.HTTPConnection("api.booli.se") #python 3
-connection = httplib.HTTPConnection("api.booli.se") #python 2
-connection.request("GET", url)
-response = connection.getresponse()
-data = response.read()
-connection.close()
-
-if response.status != 200:
-    print("fail")
-
-parsed = json.loads(data)
-output = parsed
-
-totalCount = parsed["totalCount"]
-
-print("totalCount = "+str(totalCount))
-
-noRequests = totalCount / limit + 1
-
-for i in range(1, noRequests): 
-  offset = i * limit
-  url = "/sold?q="+query+"&limit="+str(limit)+"&offset="+str(offset)+"&callerId="+callerId+"&time="+timestamp+"&unique="+unique+"&hash="+hashstr
-  connection = httplib.HTTPConnection("api.booli.se") #python 2
-  connection.request("GET", url)
-  response = connection.getresponse()
-  data = response.read()
-  connection.close()
-  parsed = json.loads(data)
-  count = parsed["count"]
-  for i in range(0, count):
-    newItem = parsed["sold"][i]
-    output["sold"].append(newItem)
+print "finished"
+#print(callbooliapi(limit, query, offset))
 
 
-with open('data.json', 'w') as outfile:
-    json.dump(output["sold"], outfile, indent=4)
+
+
+
+# parsed = json.loads(data)
+# output = parsed
+
+# totalCount = parsed["totalCount"]
+
+# print("totalCount = "+str(totalCount))
+
+# noRequests = totalCount / limit + 1
+
+# for i in range(1, noRequests): 
+#   offset = i * limit
+#   url = "/sold?q="+query+"&limit="+str(limit)+"&offset="+str(offset)+"&callerId="+callerId+"&time="+timestamp+"&unique="+unique+"&hash="+hashstr
+#   connection = httplib.HTTPConnection("api.booli.se") #python 2
+#   connection.request("GET", url)
+#   response = connection.getresponse()
+#   data = response.read()
+#   connection.close()
+#   parsed = json.loads(data)
+#   count = parsed["count"]
+#   for i in range(0, count):
+#     newItem = parsed["sold"][i]
+#     output["sold"].append(newItem)
+
+
+# with open('data.json', 'w') as outfile:
+#     json.dump(output["sold"], outfile, indent=4)
